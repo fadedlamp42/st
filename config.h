a99af2de2bebb2e4780a8da504f1955740fece67
Peter Gomez <fadedlamp42@gmail.com>
2022-04-02 17:51:47 -0400

    increase default font size; increase border spacing

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=18:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Monospace:pixelsize=24:antialias=true:autohint=true";
+static int borderpx = 4;
 
 /*
  * What program is execed by st depends of these precedence rules:
